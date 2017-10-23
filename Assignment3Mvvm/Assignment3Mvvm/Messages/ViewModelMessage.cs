7f0da4363db272369e80db08befba93f308246bd
anthonyrojas <anthony.rojas095@gmail.com>
2017-10-23 14:10:17 -0700

    Add files via upload

---
+++ Assignment3Mvvm/Assignment3Mvvm/Messages/ViewModelMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace Assignment3Mvvm.Messages
+{
+    public class ViewModelMessage : MessageBase
+    {
+        public string FirstNameText { get; set; }
+        public string LastNameText { get; set; }
+        public string EmailText { get; set; }
+        public string WindowCommandText { get; set; }
+    }
+}
