7c7e36c5c88f9a8575b07c66737075132bdaf94e
anthonyrojas <anthony.rojas095@gmail.com>
2017-11-07 17:14:52 -0800

    Add files via upload

---
+++ Assignment5/DataAccessLayer/Repository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Data.Entity;
+
+namespace DataAccessLayer
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext context;
+        protected DbSet<T> dbset;
+        public Repository(DbContext datacontext)
+        {
+            //You can use the cpmt
+            this.context = datacontext;
+            dbset = context.Set<T>();
+
+        }
+
+        public void Insert(T entity)
+        {
+            //Use the context object and entity state to save the entity
+            context.Entry(entity).State = EntityState.Added;
+            context.SaveChanges();
+
+        }
+
+        public void Delete(T entity)
+        {
+            //Use the context object and entity state to delete the entity
+            context.Entry(entity).State = EntityState.Detached;
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            //Use the context object and entity state to update the entity
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return dbset.Find(id);
+        }
+
+        public IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate)
+        {
+            return dbset.Where(predicate);
+            //return context.Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbset.ToList();
+        }
+
+        //This method will find the related records by passing two argument
+        //First argument: lambda expression to search a record such as d => d.StandardName.Equals(standardName) to search am record by standard name
+        //Second argument: navigation property that leads to the related records such as d => d.Students
+        //The method returns the related records that met the condition in the first argument.
+        //An example of the method GetStandardByName(string standardName)
+        //public Standard GetStandardByName(string standardName)
+        //{
+        //return _standardRepository.GetSingle(d => d.StandardName.Equals(standardName), d => d.Students);
+        //}
+        public T GetSingle(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            T item = null;
+            IQueryable<T> dbQuery = context.Set<T>();
+            foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                dbQuery = dbQuery.Include<T, object>(navigationProperty);
+            item = dbQuery.AsNoTracking().FirstOrDefault(where);
+            return item;
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
